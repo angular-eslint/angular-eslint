2754d341c69bea022a2b1b39ca83b790a79591ec
Fabriece Sumuni <sfabriece@hotmail.com>
2019-07-31 01:48:01 +0200

    feat(eslint-plugin): prefer onpush component cd

---
+++ packages/eslint-plugin/src/rules/prefer-on-push-component-change-detection.ts
@@ -0,0 +1,52 @@
+import { TSESTree } from '@typescript-eslint/experimental-utils';
+import { createESLintRule } from '../utils/create-eslint-rule';
+import { COMPONENT_CLASS_DECORATOR } from '../utils/selectors';
+import { getDecoratorPropertyValue } from '../utils/utils';
+
+type Options = [];
+export type MessageIds = 'preferOnPushComponentChangeDetection';
+export const RULE_NAME = 'prefer-on-push-component-change-detection';
+
+const ON_PUSH = 'OnPush';
+
+export default createESLintRule<Options, MessageIds>({
+  name: RULE_NAME,
+  meta: {
+    type: 'suggestion',
+    docs: {
+      description: `Enforces component's change detection to ChangeDetectionStrategy.${ON_PUSH}.`,
+      category: 'Best Practices',
+      recommended: false,
+    },
+    schema: [],
+    messages: {
+      preferOnPushComponentChangeDetection: `The changeDetection value of a component should be set to ChangeDetectionStrategy.${ON_PUSH}`,
+    },
+  },
+  defaultOptions: [],
+  create(context) {
+    return {
+      [COMPONENT_CLASS_DECORATOR](node: TSESTree.Decorator) {
+        const changeDetectionExpression = getDecoratorPropertyValue(
+          node,
+          'changeDetection',
+        ) as TSESTree.MemberExpression;
+
+        if (!changeDetectionExpression) {
+          context.report({
+            node,
+            messageId: 'preferOnPushComponentChangeDetection',
+          });
+        } else if (
+          (changeDetectionExpression.property as TSESTree.Identifier).name !==
+          ON_PUSH
+        ) {
+          context.report({
+            node: changeDetectionExpression,
+            messageId: 'preferOnPushComponentChangeDetection',
+          });
+        }
+      },
+    };
+  },
+});
