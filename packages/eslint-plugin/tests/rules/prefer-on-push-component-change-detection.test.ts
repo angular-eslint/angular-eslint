23d5a6f8db4595ad5239c69dc61c6d03f4d244d2
Fabriece Sumuni <sfabriece@users.noreply.github.com>
2019-07-31 03:34:24 +0200

    feat(eslint-plugin): prefer onpush component cd (#7)

---
+++ packages/eslint-plugin/tests/rules/prefer-on-push-component-change-detection.test.ts
@@ -0,0 +1,56 @@
+import rule, {
+  MessageIds,
+  RULE_NAME,
+} from '../../src/rules/prefer-on-push-component-change-detection';
+import {
+  convertAnnotatedSourceToFailureCase,
+  RuleTester,
+} from '../test-helper';
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+const ruleTester = new RuleTester({
+  parser: '@typescript-eslint/parser',
+});
+
+const messageId: MessageIds = 'preferOnPushComponentChangeDetection';
+
+ruleTester.run(RULE_NAME, rule, {
+  valid: [
+    // should succeed if ChangeDetectionStrategy.OnPush is set
+    `
+    @Component({
+      changeDetection: ChangeDetectionStrategy.OnPush
+    })
+    class Test {}
+    `,
+  ],
+  invalid: [
+    convertAnnotatedSourceToFailureCase({
+      description: 'should fail if ChangeDetectionStrategy.Default is set',
+      annotatedSource: `
+      @Component({
+        changeDetection: ChangeDetectionStrategy.Default
+                         ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+      })
+      class Test {}
+      `,
+      messageId,
+    }),
+    convertAnnotatedSourceToFailureCase({
+      description:
+        'should fail if no ChangeDetectionStrategy is explicitly set',
+      annotatedSource: `
+      @Component({
+      ~~~~~~~~~~~~
+        selector: 'foo'
+      })
+      ~~
+      class Test {}
+      `,
+      messageId,
+    }),
+  ],
+});
