b5a5dba8549f2071245f00649119796efd181e8b
BO41 <botbob@disroot.org>
2020-03-02 18:21:24 +0000

    fix(eslint-plugin-template): missing rule export "no-negated-async"  (#46)

--- packages/eslint-plugin-template/src/index.ts
+++ packages/eslint-plugin-template/src/index.ts
@@ -2,10 +2,14 @@ import processors from './processors';
 import bananaInABox, {
   RULE_NAME as bananaInABoxRuleName,
 } from './rules/banana-in-a-box';
+import noNegatedAsync, {
+  RULE_NAME as noNegatedAsyncRuleName,
+} from './rules/no-negated-async';
 
 export default {
   processors,
   rules: {
     [bananaInABoxRuleName]: bananaInABox,
+    [noNegatedAsyncRuleName]: noNegatedAsync,
   },
 };
