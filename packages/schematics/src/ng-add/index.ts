46d997fd0170f6bc4935e9480fb7d172245d0b09
James Henry <james@henry.sc>
2020-03-20 20:22:50 +0000

    feat(schematics): add new schematics package

---
+++ packages/schematics/src/ng-add/index.ts
@@ -0,0 +1,41 @@
+import {
+  Rule,
+  SchematicContext,
+  Tree,
+  chain,
+} from '@angular-devkit/schematics';
+import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
+import packageJSON from '../../package.json';
+
+function addAngularESLintPackages() {
+  return (host: Tree, context: SchematicContext) => {
+    if (!host.exists('package.json')) {
+      throw new Error(
+        'Could not locate package.json at the root of your project',
+      );
+    }
+
+    const projectPackageJSON = host.read('package.json')!.toString('utf-8');
+    const json = JSON.parse(projectPackageJSON);
+
+    json.devDependencies = json.devDependencies || {};
+    json.devDependencies['@angular-eslint/builder'] = packageJSON.version;
+    json.devDependencies['@angular-eslint/eslint-plugin'] = packageJSON.version;
+    json.devDependencies['@angular-eslint/eslint-plugin-template'] =
+      packageJSON.version;
+    json.devDependencies['@angular-eslint/template-parser'] =
+      packageJSON.version;
+
+    host.overwrite('package.json', JSON.stringify(json, null, 2));
+
+    context.addTask(new NodePackageInstallTask());
+
+    return host;
+  };
+}
+
+export default function(): Rule {
+  return (host: Tree, context: SchematicContext) => {
+    return chain([addAngularESLintPackages()])(host, context);
+  };
+}
