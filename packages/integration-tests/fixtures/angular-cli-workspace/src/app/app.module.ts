696ef7baa21bac69e68adb5413462cb0ebdc4989
James Henry <james@henry.sc>
2020-04-22 10:30:56 +0100

    fix(eslint-plugin-template): false positive on multiple decorators

--- packages/integration-tests/fixtures/angular-cli-workspace/src/app/app.module.ts
+++ packages/integration-tests/fixtures/angular-cli-workspace/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ExamplePipe } from './example.pipe';
 import { ExampleComponent } from './example.component';
+import { Regression73Component } from './regression-73.component';
 
 @NgModule({
-  declarations: [AppComponent, ExamplePipe, ExampleComponent],
+  declarations: [AppComponent, ExamplePipe, ExampleComponent, Regression73Component],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
