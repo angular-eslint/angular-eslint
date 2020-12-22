2d33478bdbc4bbba960a84073bb540679838a22d
Rafael Santana <rafaelsantana3095@gmail.com>
2020-12-22 15:12:47 -0300

    refactor(eslint-plugin-template): no-call-expression switch to selectors (#253)

--- packages/eslint-plugin-template/src/rules/no-call-expression.ts
+++ packages/eslint-plugin-template/src/rules/no-call-expression.ts
@@ -1,52 +1,47 @@
+import { MethodCall } from '@angular/compiler';
 import {
   createESLintRule,
-  getTemplateParserServices,
+  ensureTemplateParser,
 } from '../utils/create-eslint-rule';
 
 type Options = [];
 export type MessageIds = 'noCallExpression';
 export const RULE_NAME = 'no-call-expression';
 
-const ALLOWED_METHOD_NAMES: ReadonlySet<string> = new Set(['$any']);
-
 export default createESLintRule<Options, MessageIds>({
   name: RULE_NAME,
   meta: {
     type: 'suggestion',
     docs: {
-      description: `Disallows calling expressions in templates, except for output handlers.`,
+      description:
+        'Disallows calling expressions in templates, except for output handlers',
       category: 'Best Practices',
       recommended: false,
     },
     schema: [],
     messages: {
-      noCallExpression: 'Avoid calling expressions in templates.',
+      noCallExpression: 'Avoid calling expressions in templates',
     },
   },
   defaultOptions: [],
   create(context) {
-    const parserServices = getTemplateParserServices(context);
+    ensureTemplateParser(context);
     const sourceCode = context.getSourceCode();
 
-    return parserServices.defineTemplateBodyVisitor({
-      MethodCall(node: any) {
-        const isMethodAllowed = ALLOWED_METHOD_NAMES.has(node.name);
-
-        if (isMethodAllowed || node.parent.parent.type === 'BoundEvent') {
-          return;
-        }
-
-        const start = sourceCode.getLocFromIndex(node.sourceSpan.start);
-        const end = sourceCode.getLocFromIndex(node.sourceSpan.end);
+    return {
+      ':not(BoundEvent) > * > MethodCall[name!="$any"]'({
+        sourceSpan: { end, start },
+      }: MethodCall) {
+        const loc = {
+          start: sourceCode.getLocFromIndex(start),
+          end: sourceCode.getLocFromIndex(end),
+        } as const;
 
         context.report({
           messageId: 'noCallExpression',
-          loc: {
-            start,
-            end,
-          },
+          loc,
         });
       },
-    });
+    };
   },
 });
