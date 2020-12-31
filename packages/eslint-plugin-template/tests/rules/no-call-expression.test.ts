7f85ed7cd0b58fa1f9c6c8b83c9790409c585c6e
Rafael Santana <rafaelsantana3095@gmail.com>
2020-12-31 08:22:35 -0300

    fix(eslint-plugin-template): not reporting failures for safe method calls (#269)
    Co-authored-by: James Henry <james@henry.sc>

--- packages/eslint-plugin-template/tests/rules/no-call-expression.test.ts
+++ packages/eslint-plugin-template/tests/rules/no-call-expression.test.ts
@@ -21,17 +21,15 @@ ruleTester.run(RULE_NAME, rule, {
   valid: [
     `
       {{ info }}
-    `,
-    `
       <button type="button" (click)="handleClick()">Click Here</button>
-    `,
-    `
       {{ $any(info) }}
+      <input (change)="obj?.changeHandler()">
     `,
   ],
   invalid: [
     convertAnnotatedSourceToFailureCase({
-      description: 'it should fail with call expression in expression binding',
+      description:
+        'it should fail for call expression in an expression binding',
       annotatedSource: `
         <div>{{ getInfo() }}</div>
                 ~~~~~~~~~
@@ -39,16 +37,8 @@ ruleTester.run(RULE_NAME, rule, {
       messageId,
     }),
     convertAnnotatedSourceToFailureCase({
-      description: 'it should fail with call expression in expression binding',
-      annotatedSource: `
-        <a href="http://example.com">{{ getInfo() }}</a>
-                                        ~~~~~~~~~
-      `,
-      messageId,
-    }),
-    convertAnnotatedSourceToFailureCase({
       description:
-        'it should fail when using a property resulted from a call expression in expression binding',
+        'it should fail when using a property resulted from a call expression in an expression binding',
       annotatedSource: `
         <a href="http://example.com">{{ getInfo().name }}</a>
                                         ~~~~~~~~~
@@ -56,7 +46,7 @@ ruleTester.run(RULE_NAME, rule, {
       messageId,
     }),
     convertAnnotatedSourceToFailureCase({
-      description: 'it should fail with call expression in property binding',
+      description: 'it should fail for call expression in a property binding',
       annotatedSource: `
         <a [href]="getUrl()">info</a>
                    ~~~~~~~~
@@ -64,12 +54,21 @@ ruleTester.run(RULE_NAME, rule, {
       messageId,
     }),
     convertAnnotatedSourceToFailureCase({
-      description: 'it should fail with a property access call expression',
+      description: 'it should fail for safe/unsafe method calls',
       annotatedSource: `
-        <a [href]="helper.getUrl()">info</a>
-                   ~~~~~~~~~~~~~~~
+        {{ obj?.nested1() }} {{ obj!.nested1() }}
+            ~~~~~~~~~~~~~~       ^^^^^^^^^^^^^^      
+        <button [type]="obj!.$any(b)!.getType()">info</button>
+                        #######################
+        <a [href]="obj.propertyA?.href()">info</a>
+                   %%%%%%%%%%%%%%%%%%%%%
       `,
-      messageId,
+      messages: [
+        { char: '~', messageId },
+        { char: '^', messageId },
+        { char: '#', messageId },
+        { char: '%', messageId },
+      ],
     }),
   ],
 });
