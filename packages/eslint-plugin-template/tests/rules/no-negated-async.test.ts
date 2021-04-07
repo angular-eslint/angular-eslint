1766ce5acf91684c3793740e07e77a6e603c5ce0
Rafael Santana <rafaelsantana3095@gmail.com>
2021-04-07 13:43:53 -0300

    refactor(eslint-plugin-template): no-negated-async no longer perform equality checks

--- packages/eslint-plugin-template/tests/rules/no-negated-async.test.ts
+++ packages/eslint-plugin-template/tests/rules/no-negated-async.test.ts
@@ -12,28 +12,20 @@ import rule, { RULE_NAME } from '../../src/rules/no-negated-async';
 const ruleTester = new RuleTester({
   parser: '@angular-eslint/template-parser',
 });
-
-const noLooseEquality: MessageIds = 'noLooseEquality';
-const noNegatedAsync: MessageIds = 'noNegatedAsync';
+const messageId: MessageIds = 'noNegatedAsync';
 
 ruleTester.run(RULE_NAME, rule, {
   valid: [
     // it should succeed if async pipe is not negated
-    `
-      {{ (foo | async) }}
-    `,
+    `{{ (foo | async) }}`,
     // it should succeed if async pipe is not the last pipe in the negated chain
-    `
-      {{ !(foo | async | somethingElse) }}
-    `,
+    `{{ !(foo | async | somethingElse) }}`,
+    // it should succeed if async pipe uses loose equality
+    `{{ (foo | async) == null }}`,
     // it should succeed if async pipe uses strict equality
-    `
-      {{ (foo | async) === false }}
-    `,
+    `{{ (foo | async) === false }}`,
     // it should succeed if any other pipe is negated
-    `
-      {{ !(foo | notAnAsyncPipe) }}
-    `,
+    `{{ !(foo | notAnAsyncPipe) }}`,
   ],
   invalid: [
     convertAnnotatedSourceToFailureCase({
@@ -46,28 +38,16 @@ ruleTester.run(RULE_NAME, rule, {
         {{      !(foo | async) }}
                 ~~~~~~~~~~~~~~
       `,
-      messageId: noNegatedAsync,
+      messageId,
     }),
     convertAnnotatedSourceToFailureCase({
       description:
         'it should fail if async pipe is the last pipe in the negated chain',
       annotatedSource: `
-          {{ !(foo | somethingElse | async) }}
-             ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        {{ !(foo | somethingElse | async) }}
+           ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
       `,
-      messageId: noNegatedAsync,
-    }),
-    convertAnnotatedSourceToFailureCase({
-      description: 'it should fail if the async pipe uses loose equality',
-      /**
-       * Deliberately including some leading whitespace within the binding to assert
-       * location correctness
-       */
-      annotatedSource: `
-        {{    (foo | async) == false }}
-              ~~~~~~~~~~~~~~~~~~~~~~
-      `,
-      messageId: noLooseEquality,
+      messageId,
     }),
     convertAnnotatedSourceToFailureCase({
       description: 'it should fail if async pipe is negated using *ngIf',
@@ -75,30 +55,7 @@ ruleTester.run(RULE_NAME, rule, {
         <div *ngIf="!(a | async)"></div>
                     ~~~~~~~~~~~~
       `,
-      messageId: noNegatedAsync,
-    }),
-    convertAnnotatedSourceToFailureCase({
-      description:
-        'it should fail for multiple negated/loose equality async pipes',
-      annotatedSource: `
-        <div *ngFor="let elem of [1, 2, 3]; trackBy: trackByFn">
-          {{ elem }}
-        </div>
-        <div *ngIf="!(foo | async)">
-                    ~~~~~~~~~~~~~~
-          {{ (foo | async) == false }}
-             ^^^^^^^^^^^^^^^^^^^^^^
-          <div *ngIf="(foo | async) == false">
-                      ######################
-            works!
-          </div>
-        </div>
-      `,
-      messages: [
-        { char: '~', messageId: noNegatedAsync },
-        { char: '^', messageId: noLooseEquality },
-        { char: '#', messageId: noLooseEquality },
-      ],
+      messageId,
     }),
   ],
 });
