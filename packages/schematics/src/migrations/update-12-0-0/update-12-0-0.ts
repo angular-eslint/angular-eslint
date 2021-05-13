880b2a5f2c10c93700b2793e583c63f097da8d69
Rafael Santana <rafaelsantana3095@gmail.com>
2021-05-13 12:53:37 -0300

    chore: more v12 migrations (#447)

--- packages/schematics/src/migrations/update-12-0-0/update-12-0-0.ts
+++ packages/schematics/src/migrations/update-12-0-0/update-12-0-0.ts
@@ -1,5 +1,7 @@
 import type { Rule } from '@angular-devkit/schematics';
 import { chain } from '@angular-devkit/schematics';
+import type { Linter } from 'eslint';
+import { updateJsonInTree, visitNotIgnoredFiles } from '../../utils';
 import { updateDependencies } from '../utils/dependencies';
 
 const updatedAngularESLintVersion = '^12.0.0';
@@ -9,6 +11,87 @@ const packageJSON = require('../../../package.json');
 const updatedTypeScriptESLintVersion =
   packageJSON.devDependencies['@typescript-eslint/experimental-utils'];
 const updatedESLintVersion = packageJSON.devDependencies['eslint'];
+
+function migrateToAccessibilityLabelHasAssociatedControlSchema(
+  rule: Linter.RuleEntry | undefined,
+) {
+  if (!Array.isArray(rule) || rule.length !== 2) return;
+  const [, currentSchema] = rule;
+  rule[1] = {
+    controlComponents: currentSchema.controlComponents,
+    labelComponents: currentSchema.labelComponents.map((selector: string) => {
+      return { inputs: currentSchema.labelAttributes, selector };
+    }, []),
+  };
+}
+
+function migrateFromAccessibilityLabelFor({ overrides, rules }: Linter.Config) {
+  migrateToAccessibilityLabelHasAssociatedControlSchema(
+    rules?.['@angular-eslint/template/accessibility-label-for'],
+  );
+  migrateToAccessibilityLabelHasAssociatedControlName(rules);
+  for (const override of overrides ?? []) {
+    migrateToAccessibilityLabelHasAssociatedControlSchema(
+      override.rules?.['@angular-eslint/template/accessibility-label-for'],
+    );
+    migrateToAccessibilityLabelHasAssociatedControlName(override.rules);
+  }
+}
+
+function migrateToAccessibilityLabelHasAssociatedControlName(
+  rules: Partial<Linter.RulesRecord> | undefined,
+) {
+  if (!rules) return;
+  const accessibilityLabelForRule =
+    rules['@angular-eslint/template/accessibility-label-for'];
+  delete rules['@angular-eslint/template/accessibility-label-for'];
+  rules[
+    '@angular-eslint/template/accessibility-label-has-associated-control'
+  ] = accessibilityLabelForRule;
+}
+
+function updateAccessibilityLabelFor() {
+  return chain([
+    visitNotIgnoredFiles((filePath) => {
+      if (!filePath.endsWith('.eslintrc.json')) {
+        return;
+      }
+      return updateJsonInTree(filePath.toString(), (json) => {
+        migrateFromAccessibilityLabelFor(json);
+        return json;
+      });
+    }),
+  ]);
+}
+
+function addEqeqeqIfNeeded(rules: Partial<Linter.RulesRecord> | undefined) {
+  if (
+    !rules ||
+    !rules['@angular-eslint/template/no-negated-async'] ||
+    rules['@angular-eslint/template/eqeqeq']
+  ) {
+    return;
+  }
+
+  rules['@angular-eslint/template/eqeqeq'] = 'error';
+}
+
+function addEqeqeq() {
+  return chain([
+    visitNotIgnoredFiles((filePath) => {
+      if (!filePath.endsWith('.eslintrc.json')) {
+        return;
+      }
+      return updateJsonInTree(filePath.toString(), (json) => {
+        addEqeqeqIfNeeded(json.rules);
+        (json.overrides ?? []).forEach((override: Linter.ConfigOverride) =>
+          addEqeqeqIfNeeded(override.rules),
+        );
+        return json;
+      });
+    }),
+  ]);
+}
 
 export default function migration(): Rule {
   return chain([
@@ -46,5 +129,7 @@ export default function migration(): Rule {
         version: updatedESLintVersion,
       },
     ]),
+    updateAccessibilityLabelFor,
+    addEqeqeq,
   ]);
 }
