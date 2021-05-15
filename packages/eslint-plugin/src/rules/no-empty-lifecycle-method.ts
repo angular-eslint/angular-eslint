67cfdbf3e13b2566bab46d9b61cf338c39962031
Rafael Santana <rafaelsantana3095@gmail.com>
2021-05-15 16:21:11 -0300

    feat(eslint-plugin): [no-empty-lifecycle-method] add suggestion

--- packages/eslint-plugin/src/rules/no-empty-lifecycle-method.ts
+++ packages/eslint-plugin/src/rules/no-empty-lifecycle-method.ts
@@ -3,11 +3,16 @@ import { createESLintRule } from '../utils/create-eslint-rule';
 import {
   ANGULAR_LIFECYCLE_METHODS,
   getAngularClassDecorator,
+  getImplementsRemoveFix,
+  getImportDeclarations,
+  getImportRemoveFix,
   toPattern,
 } from '../utils/utils';
 
 type Options = [];
-export type MessageIds = 'noEmptyLifecycleMethod';
+export type MessageIds =
+  | 'noEmptyLifecycleMethod'
+  | 'suggestRemoveLifecycleMethod';
 export const RULE_NAME = 'no-empty-lifecycle-method';
 
 export default createESLintRule<Options, MessageIds>({
@@ -18,14 +23,17 @@ export default createESLintRule<Options, MessageIds>({
       description: 'Disallows declaring empty lifecycle methods',
       category: 'Best Practices',
       recommended: 'error',
+      suggestion: true,
     },
     schema: [],
     messages: {
       noEmptyLifecycleMethod: 'Lifecycle methods should not be empty',
+      suggestRemoveLifecycleMethod: 'Remove lifecycle method',
     },
   },
   defaultOptions: [],
   create(context) {
+    const sourceCode = context.getSourceCode();
     const angularLifecycleMethodsPattern = toPattern([
       ...ANGULAR_LIFECYCLE_METHODS,
     ]);
@@ -33,6 +41,7 @@ export default createESLintRule<Options, MessageIds>({
     return {
       [`ClassDeclaration > ClassBody > MethodDefinition[key.name=${angularLifecycleMethodsPattern}][value.body.body.length=0]`](
         node: TSESTree.MethodDefinition & {
+          key: TSESTree.Identifier;
           parent: TSESTree.ClassBody & { parent: TSESTree.ClassDeclaration };
         },
       ) {
@@ -41,6 +50,33 @@ export default createESLintRule<Options, MessageIds>({
         context.report({
           node,
           messageId: 'noEmptyLifecycleMethod',
+          suggest: [
+            {
+              messageId: 'suggestRemoveLifecycleMethod',
+              fix: (fixer) => {
+                const importDeclarations = getImportDeclarations(
+                  node,
+                  '@angular/core',
+                );
+                const interfaceName = node.key.name.replace(/^ng+/, '');
+
+                return [fixer.remove(node)].concat(
+                  getImplementsRemoveFix(
+                    sourceCode,
+                    node.parent.parent,
+                    interfaceName,
+                    fixer,
+                  ),
+                  getImportRemoveFix(
+                    sourceCode,
+                    importDeclarations ?? [],
+                    interfaceName,
+                    fixer,
+                  ),
+                );
+              },
+            },
+          ],
         });
       },
     };
