603ea7fc979cd0a1f24ad2f590a84115e607db08
Rafael Santana <rafaelsantana3095@gmail.com>
2021-05-31 12:36:32 -0300

    fix(eslint-plugin): [no-output-on-prefix] correct false positives

--- packages/eslint-plugin/src/rules/no-output-on-prefix.ts
+++ packages/eslint-plugin/src/rules/no-output-on-prefix.ts
@@ -13,7 +13,7 @@ export default createESLintRule<Options, MessageIds>({
   meta: {
     type: 'suggestion',
     docs: {
-      description: `Ensures that Output bindings, including aliases, are not named "on", nor prefixed with it. See more at ${STYLE_GUIDE_LINK}`,
+      description: `Ensures that output bindings, including aliases, are not named "on", nor prefixed with it. See more at ${STYLE_GUIDE_LINK}`,
       category: 'Best Practices',
       recommended: 'error',
     },
@@ -24,12 +24,14 @@ export default createESLintRule<Options, MessageIds>({
   },
   defaultOptions: [],
   create(context) {
-    const outputAliasSelector = `ClassProperty:has(${OUTPUT_DECORATOR}[expression.arguments.0.value=${OUTPUT_ON_PATTERN}])`;
-    const outputPropertySelector = `ClassProperty:has(${OUTPUT_DECORATOR}):has(Identifier[name=${OUTPUT_ON_PATTERN}])`;
+    const outputAliasSelector = `ClassProperty ${OUTPUT_DECORATOR} :matches(Literal[value=${OUTPUT_ON_PATTERN}], TemplateElement[value.raw=${OUTPUT_ON_PATTERN}])`;
+    const outputPropertySelector = `ClassProperty[computed=false]:has(${OUTPUT_DECORATOR}) > :matches(Identifier[name=${OUTPUT_ON_PATTERN}], Literal[value=${OUTPUT_ON_PATTERN}])`;
     const selectors = [outputAliasSelector, outputPropertySelector].join(',');
 
     return {
-      [selectors](node: TSESTree.ClassProperty) {
+      [selectors](
+        node: TSESTree.Identifier | TSESTree.Literal | TSESTree.TemplateElement,
+      ) {
         context.report({
           node,
           messageId: 'noOutputOnPrefix',
