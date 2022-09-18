42cd39875e6c71ab247c6ad12b0f1ca837cb8603
Sandi Barr <sandi.k.barr@gmail.com>
2022-09-18 10:20:25 -0500

    feat(eslint-plugin-template): [accessibility-role-has-required-aria] lazy load axobject-query

--- packages/eslint-plugin-template/src/utils/is-semantic-role-element.ts
+++ packages/eslint-plugin-template/src/utils/is-semantic-role-element.ts
@@ -2,36 +2,50 @@ import type {
   TmplAstBoundAttribute,
   TmplAstTextAttribute,
 } from '@angular-eslint/bundled-angular-compiler';
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const { AXObjectRoles, elementAXObjects } = require('axobject-query');
 
+// The axobject-query package doesn't have type definitions, but this is what we're using from it here
+let axElements: Map<
+  { name: string; attributes?: { name: string; value: string }[] },
+  string[]
+> | null = null;
+let axRoles: Map<string, { name: string }[]> | null = null;
+
+// This function follows the lazy initialization pattern.
+// Since this is a top-level module (it will be included via `require`), we do not need to
+// initialize the `nonInteractiveElementRoleSchemas` until the function is called
+// for the first time, so we will not take up the memory.
 export function isSemanticRoleElement(
   element: string,
   role: string,
   elementAttributes: (TmplAstTextAttribute | TmplAstBoundAttribute)[],
 ): boolean {
+  if (axElements === null || axRoles === null) {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const { AXObjectRoles, elementAXObjects } = require('axobject-query');
+    axElements = elementAXObjects;
+    axRoles = AXObjectRoles;
+  }
+
   // elementAXObjects: HTML elements are mapped to their related AXConcepts concepts
-  return elementAXObjects.keys().some(
-    (htmlElement: {
-      name: string;
-      attributes?: { name: string; value: string }[];
-    }) =>
+  return Array.from(axElements?.keys() ?? []).some(
+    (htmlElement) =>
       htmlElement.name === element &&
-      (htmlElement.attributes || []).every(
-        (htmlElemAttr: { name: string; value: string }) =>
-          // match given elementAttributes to htmlElement attributes
-          elementAttributes.some(
-            (elemAttr) =>
-              htmlElemAttr.name === elemAttr.name &&
-              htmlElemAttr.value === elemAttr.value,
-          ),
+      (htmlElement.attributes ?? []).every((htmlElemAttr) =>
+        // match every axElement html attributes to given elementAttributes
+        elementAttributes.find(
+          (elemAttr) =>
+            htmlElemAttr.name === elemAttr.name &&
+            htmlElemAttr.value === elemAttr.value,
+        ),
       ) &&
       // aria- properties are covered by the element's semantic role
-      elementAXObjects.get(htmlElement)?.some((roleName: unknown) =>
+      axElements?.get(htmlElement)?.find((roleName: string) =>
         // AXObjectRoles: AXObjects are mapped to their related ARIA concepts
-        AXObjectRoles.get(roleName)?.some(
-          (semanticRole: { name: string }) => semanticRole.name === role,
-        ),
+        axRoles
+          ?.get(roleName)
+          ?.find(
+            (semanticRole: { name: string }) => semanticRole.name === role,
+          ),
       ),
   );
 }
