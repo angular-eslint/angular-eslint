d17ff3394791dd6a4a49e659956bf6fc1d456aeb
James Henry <james@henry.sc>
2022-11-20 15:12:36 +0400

    chore: revert packages/eslint-plugin-template/src/utils/create-eslint-rule.ts

--- packages/eslint-plugin-template/src/utils/create-eslint-rule.ts
+++ packages/eslint-plugin-template/src/utils/create-eslint-rule.ts
@@ -4,9 +4,7 @@ import { ESLintUtils } from '@typescript-eslint/utils';
  * We need to patch the RuleCreator in order to preserve the defaultOptions
  * to use as part of documentation generation.
  */
-const patchedRuleCreator: typeof ESLintUtils.RuleCreator = (
-  urlCreator,
-) => {
+const patchedRuleCreator: typeof ESLintUtils.RuleCreator = (urlCreator) => {
   return function createRule({ name, meta, defaultOptions, create }) {
     return {
       meta: Object.assign(Object.assign({}, meta), {
