24096cf4b01545bf01de1b9a72ba390435c86289
“JamesHenry” <james@henry.sc>
2023-09-03 15:09:12 +0400

    chore: update test based on library changes

--- packages/eslint-plugin-template/tests/rules/interactive-supports-focus/cases.ts
+++ packages/eslint-plugin-template/tests/rules/interactive-supports-focus/cases.ts
@@ -58,8 +58,8 @@ export const valid = [
       <button (click)="onClick()" class="foo">Foo</button>
       <option (click)="onClick()" class="foo">Food</option>
       <select (click)="onClick()" class="foo"></select>
-      <summary (click)="onClick()">Foo</summary>
-      <textarea (keypress)="onKeypress()" class="foo"></textarea>
+      <summary (click)="onClick()" aria-expanded>Foo</summary>
+      <textarea (keypress)="onKeypress()" class="foo" aria-multiline></textarea>
     `,
   },
 
