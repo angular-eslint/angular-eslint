5c6fd94f4a9b6bfcdf5d4e96903e17dfdc6dc6de
arturovt <arthurandrosovich@gmail.com>
2023-09-16 21:46:39 +0300

    test: address feedback and add more test cases

--- packages/eslint-plugin-template/tests/rules/use-track-by-function/cases.ts
+++ packages/eslint-plugin-template/tests/rules/use-track-by-function/cases.ts
@@ -68,6 +68,14 @@ export const valid = [
       <div *ngFor="let photo of photos; trackById"></div>
     `,
   },
+  {
+    options: [{ alias: ['ngForTrackByProperty'] }],
+    code: `
+      <ng-template ngFor let-item [ngForOf]="[1, 2, 3]" let-i="index" [ngForTrackByProperty]="trackByFn">
+        {{ item }}
+      </ng-template>
+    `,
+  },
 ];
 
 export const invalid = [
@@ -84,7 +92,33 @@ export const invalid = [
     messageId,
   }),
   convertAnnotatedSourceToFailureCase({
+    description:
+      'should fail when trackBy function (`ngForTrackByProperty` alias) is not present',
+    options: [{ alias: ['ngForTrackByProperty'] }],
+    annotatedSource: `
+        <ul>
+          <li *ngFor="let item of [1, 2, 3];">
+              ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+            {{ item }}
+          </li>
+        </ul>
+      `,
+    messageId,
+  }),
+  convertAnnotatedSourceToFailureCase({
     description: 'should fail when [ngForTrackBy] is missing in ng-template',
+    annotatedSource: `
+        <ng-template ngFor let-item [ngForOf]="[1, 2, 3]" let-i="index">
+                                    ~~~~~~~~~~~~~~~~~~~~~
+          {{ item }}
+        </ng-template>
+      `,
+    messageId,
+  }),
+  convertAnnotatedSourceToFailureCase({
+    description:
+      'should fail when [ngForTrackByProperty] is missing in ng-template',
+    options: [{ alias: ['ngForTrackByProperty'] }],
     annotatedSource: `
         <ng-template ngFor let-item [ngForOf]="[1, 2, 3]" let-i="index">
                                     ~~~~~~~~~~~~~~~~~~~~~
@@ -132,5 +166,25 @@ export const invalid = [
         messageId,
       },
     ],
+  }),
+  convertAnnotatedSourceToFailureCase({
+    description:
+      'should fail when there are three ngFor and for the third, trackBy function (`ngForTrackByProperty` alias) is not present',
+    options: [{ alias: ['ngForTrackByProperty'] }],
+    annotatedSource: `
+        <div *ngFor="let item of [1, 2, 3]; trackBy: trackByFn">
+          {{ item }}
+        </div>
+        <div *ngFor="let item of [1, 2, 3]; trackByProperty: trackByFn">
+          {{ item }}
+        </div>
+        <ul>
+          <li *ngFor="let item of [1, 2, 3];">
+              ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+            {{ item }}
+          </li>
+        </ul>
+      `,
+    messageId,
   }),
 ];
