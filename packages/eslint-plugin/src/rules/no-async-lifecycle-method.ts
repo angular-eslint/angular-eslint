9e51559473b24ff6fa4ed12a92867fb38b8fad83
chimurai <655241+chimurai@users.noreply.github.com>
2023-11-26 11:28:16 +0000

    feat(eslint-plugin): [no-async-lifecycle-method] add rule

---
+++ packages/eslint-plugin/src/rules/no-async-lifecycle-method.ts
@@ -0,0 +1,40 @@
+import { ASTUtils, toPattern } from '@angular-eslint/utils';
+import type { TSESTree } from '@typescript-eslint/utils';
+import { createESLintRule } from '../utils/create-eslint-rule';
+
+type Options = [];
+export type MessageIds = 'noAsyncLifecycleMethod';
+export const RULE_NAME = 'no-async-lifecycle-method';
+
+export default createESLintRule<Options, MessageIds>({
+  name: RULE_NAME,
+  meta: {
+    type: 'problem',
+    docs: {
+      description: `Angular Lifecycle methods should not be async. Angular does not wait for async lifecycle but the code incorrectly suggests it does.`,
+    },
+    schema: [],
+    messages: {
+      noAsyncLifecycleMethod: 'Angular Lifecycle method should not be async',
+    },
+  },
+  defaultOptions: [],
+  create(context) {
+    const angularLifecycleMethodsPattern = toPattern([
+      ...ASTUtils.ANGULAR_LIFECYCLE_METHODS,
+    ]);
+
+    return {
+      [`ClassBody > MethodDefinition[key.name=${angularLifecycleMethodsPattern}]`](
+        node: TSESTree.MethodDefinition,
+      ): void {
+        if (node.value.async === true) {
+          context.report({
+            node: node.key,
+            messageId: 'noAsyncLifecycleMethod',
+          });
+        }
+      },
+    };
+  },
+});
