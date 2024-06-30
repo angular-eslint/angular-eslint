9103be6f7ec619ab08303e33efec4e8955e15d03
Dave <reduckted@outlook.com>
2024-07-01 04:14:42 +1000

    feat(eslint-plugin): [prefer-output-readonly] support output() function (#1876)

--- packages/eslint-plugin/src/rules/prefer-output-readonly.ts
+++ packages/eslint-plugin/src/rules/prefer-output-readonly.ts
@@ -12,13 +12,13 @@ export default createESLintRule<Options, MessageIds>({
     type: 'suggestion',
     docs: {
       description:
-        'Prefer to declare `@Output` as `readonly` since they are not supposed to be reassigned',
+        'Prefer to declare `@Output`, `OutputEmitterRef` and `OutputRef` as `readonly` since they are not supposed to be reassigned',
     },
     hasSuggestions: true,
     schema: [],
     messages: {
       preferOutputReadonly:
-        'Prefer to declare `@Output` as `readonly` since they are not supposed to be reassigned',
+        'Prefer to declare `{{type}}` as `readonly` since they are not supposed to be reassigned',
       suggestAddReadonlyModifier: 'Add `readonly` modifier',
     },
   },
@@ -28,17 +28,32 @@ export default createESLintRule<Options, MessageIds>({
       [`PropertyDefinition:not([readonly=true]) > ${Selectors.OUTPUT_DECORATOR}`]({
         parent: { key },
       }: TSESTree.Decorator & { parent: TSESTree.PropertyDefinition }) {
-        context.report({
-          node: key,
-          messageId: 'preferOutputReadonly',
-          suggest: [
-            {
-              messageId: 'suggestAddReadonlyModifier',
-              fix: (fixer) => fixer.insertTextBefore(key, 'readonly '),
-            },
-          ],
-        });
+        report(key, '@Output');
+      },
+      [`PropertyDefinition:not([readonly=true]):matches([typeAnnotation.typeAnnotation.typeName.name=OutputEmitterRef], [value.callee.name=output])`]({
+        key,
+      }: TSESTree.PropertyDefinition) {
+        report(key, 'OutputEmitterRef');
+      },
+      [`PropertyDefinition:not([readonly=true]):matches([typeAnnotation.typeAnnotation.typeName.name=OutputRef], [value.callee.name=outputFromObservable])`]({
+        key,
+      }: TSESTree.PropertyDefinition) {
+        report(key, 'OutputRef');
       },
     };
+
+    function report(key: TSESTree.Node, type: string) {
+      context.report({
+        node: key,
+        messageId: 'preferOutputReadonly',
+        data: { type },
+        suggest: [
+          {
+            messageId: 'suggestAddReadonlyModifier',
+            fix: (fixer) => fixer.insertTextBefore(key, 'readonly '),
+          },
+        ],
+      });
+    }
   },
 });
