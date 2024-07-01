05660a29b67f657f47159010908095f1c19861a6
“JamesHenry” <james@henry.sc>
2024-07-01 15:08:41 +0400

    chore: fix utils

--- packages/schematics/src/utils.ts
+++ packages/schematics/src/utils.ts
@@ -250,7 +250,7 @@ export function createStringifiedRootESLintConfig(
 const eslint = require("@eslint/js");
 const tseslint = require("typescript-eslint");
 const angular = require("angular-eslint");
-  
+
 module.exports = tseslint.config(
   {
     files: ["**/*.ts"],
