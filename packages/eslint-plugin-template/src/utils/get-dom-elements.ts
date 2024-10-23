f3247f67b88dc50e877ca2974a94670da8625707
Joan Llenas Masó <joan.llenas.maso@gmail.com>
2024-10-23 21:50:34 +0200

    fix(eslint-plugin-template): prevent the slot tag from being self-closing
    The slot tag is not included in the tags exposed by the aria-query library, which is used to extract
    native DOM elements and determine which tags can be self-closing. This fix adds the slot tag and a
    few other missing tags
    
    fix #2087

--- packages/eslint-plugin-template/src/utils/get-dom-elements.ts
+++ packages/eslint-plugin-template/src/utils/get-dom-elements.ts
@@ -1,6 +1,10 @@
 import { dom } from 'aria-query';
 
+const nonAriaElements = ['slot', 'math', 'rb', 'svg', 'template'];
+
 let domElements: ReadonlySet<string> | null = null;
 export function getDomElements(): ReadonlySet<string> {
-  return domElements ?? (domElements = new Set(dom.keys()));
+  return (
+    domElements ?? (domElements = new Set([...dom.keys(), ...nonAriaElements]))
+  );
 }
