8c289b74d06e530b835f26b9b381b9fde4a1f99b
James Henry <james@henry.sc>
2024-11-18 23:22:55 +0400

    chore: update/trim repo deps (#2099)

--- e2e/utils/run-lint.ts
+++ e2e/utils/run-lint.ts
@@ -1,27 +1,14 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 import execa from 'execa';
 import path from 'node:path';
+import { stripVTControlCharacters } from 'node:util';
 import { FIXTURES_DIR } from './fixtures';
 
-import stripAnsi = require('strip-ansi');
-
 function normalizeOutput(value: unknown): string {
-  const ansiRemoved = stripAnsi(String(value));
-  return (
-    ansiRemoved
-      .replace(
-        new RegExp(`^${FIXTURES_DIR.replace(/\\/g, '\\\\')}(.*?)$`, 'gm'),
-        (_, c1) => `__ROOT__/${c1.replace(/\\/g, '/')}`,
-      )
-      /**
-       * This one is ridiculous...
-       *
-       * No matter what I try (including the use of `strip-ansi`), I do not seem to be
-       * able to stop the inclusion of this extra `m` any other way (it is presumably
-       * coming from an ANSI code becuase of its initial appearance in CI as [27m).
-       * It happens locally and CI... but only exactly once.
-       */
-      .replace('mmax-len', 'max-len')
+  const ansiRemoved = stripVTControlCharacters(String(value));
+  return ansiRemoved.replace(
+    new RegExp(`^${FIXTURES_DIR.replace(/\\/g, '\\\\')}(.*?)$`, 'gm'),
+    (_, c1) => `__ROOT__/${c1.replace(/\\/g, '/')}`,
   );
 }
 
