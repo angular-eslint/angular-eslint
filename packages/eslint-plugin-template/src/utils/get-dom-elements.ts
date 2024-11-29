a930f58b83ff3f77da4f0f2e823fc566f7ab5c08
Joan Llenas Masó <joan.llenas.maso@gmail.com>
2024-11-29 21:35:43 +0100

    fix(eslint-plugin-template): use namespace format for svg and math

--- packages/eslint-plugin-template/src/utils/get-dom-elements.ts
+++ packages/eslint-plugin-template/src/utils/get-dom-elements.ts
@@ -1,6 +1,13 @@
 import { dom } from 'aria-query';
 
-const nonAriaElements = ['slot', 'math', 'rb', 'svg', 'template'];
+const nonAriaElements = [
+  'slot',
+  'rb',
+  'template',
+  // The Angular compiler has special handling for math and svg as namespaces. Their children have the namespace in the node name too.
+  ':svg:svg',
+  ':math:math',
+];
 
 let domElements: ReadonlySet<string> | null = null;
 export function getDomElements(): ReadonlySet<string> {
