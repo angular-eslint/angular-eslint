1aab0fba98bd2340374473328050e2d66fb714cb
JamesHenry <james@henry.sc>
2025-10-26 15:46:24 +0400

    chore: try custom distribution config for e2e

--- e2e/src/eslint-8--new-workspace-create-application-false-project-then-ng-add.test.ts
+++ e2e/src/eslint-8--new-workspace-create-application-false-project-then-ng-add.test.ts
@@ -1,6 +1,10 @@
 import path from 'node:path';
 import { setWorkspaceRoot } from 'nx/src/utils/workspace-root';
-import { FIXTURES_DIR, Fixture } from '../utils/fixtures';
+import {
+  FIXTURES_DIR,
+  Fixture,
+  resetFixtureDirectory,
+} from '../utils/fixtures';
 import {
   LONG_TIMEOUT_MS,
   runNgAdd,
@@ -20,7 +24,9 @@ describe('eslint-8--new-workspace-create-application-false-project-then-ng-add',
   jest.setTimeout(LONG_TIMEOUT_MS);
 
   beforeAll(async () => {
+    resetFixtureDirectory(fixtureDirectory);
     process.chdir(FIXTURES_DIR);
+
     await runNgNew(fixtureDirectory, false);
 
     process.env.NX_DAEMON = 'false';
